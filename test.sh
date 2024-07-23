3578b804db9fc88f781163d98f396e0586907677
Christophe Schmitz <cschmitz@redhat.com>
2024-07-23 10:12:03 +1000

    fix

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in ls AdmissionReviewExamples/*.json; do
+for i in AdmissionReviewExamples/*.json; do
     echo "=================== $i ==================="
 
     ANSWER=$(curl -k -X POST -H "Content-Type: application/json" -d @$i https://localhost:8000/mutate)
